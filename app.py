3b9dbfdea8ea6a4747529675a51fa0ad9e685b2a
Dave Springer <david.k.springer@colorado.edu>
2018-12-26 12:10:42 -0500

    Added functionality to create a new blog post

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -10,9 +10,27 @@ class BlogPost(db.Model):
     title = db.Column(db.String(120))
     body = db.Column(db.String(1000))
 
-@app.route('/')
-def hello_world():
-    return render_template('main.html')
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+@app.route('/create-new-entry')
+def add_blog_entry():
+    return render_template('new-post.html', title='Create New Post')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    if request.method == 'POST':
+        post_title = request.form.get('post-title')
+        post_body = request.form.get('post-body')
+
+        new_post = BlogPost(post_title, post_body)
+        db.session.add(new_post)
+        db.session.commit()
+
+    blog_posts = BlogPost.query.all()
+    return render_template('main.html', title='Build A Blog', blog_posts=blog_posts)
 
 
 if __name__ == '__main__':
