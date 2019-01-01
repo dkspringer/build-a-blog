6e90d4c890e23f77d702adb7fdfd9ebead23d486
Dave Springer <david.k.springer@colorado.edu>
2019-01-01 12:35:15 -0500

    Added additional CSS

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:yHXfmphcFDE7GpUc@localhost:8889/build-a-blog'
 db = SQLAlchemy(app)
 
@@ -9,10 +11,12 @@ class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(1000))
+    date = db.Column(db.DateTime)
 
     def __init__(self, title, body):
         self.title = title
         self.body = body
+        self.date = datetime.now()
 
 @app.route('/create-new-entry')
 def add_blog_entry():
